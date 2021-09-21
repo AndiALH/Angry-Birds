3e16e981140ba9930f72ad618eb7e38291938e5b
Abdurrahman Luqmanul Hakim <abdurrahman.luqmanul@ui.ac.id>
2021-09-21 22:46:16 +0700

    memasukkan project unity angry birds dan gitignore

---
+++ Assets/Scripts/BlackBird.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackBird : Bird
+{
+    [SerializeField]
+    //besar radius untuk ledakan
+    private float _explosionRadius = 2f;
+    //status apakah bird sudah meledak
+    private bool _hasExploded = false;
+
+    //Untuk efek meledak, agar simpel yang saya lakukan adalah membesarkan radius dari collider dari bird. Saat terjadi kolisi
+    //dengan objek apapun, maka collider tersebut akan membesar, dan bird akan hilang karena meledak
+    public override void OnCollisionEnter2D(Collision2D col)
+    {
+        if (!_hasExploded)
+        {
+            Debug.Log("Exploooooossionn");
+            _hasExploded = true;
+            base.OnCollisionEnter2D(col);
+            Collider.radius = _explosionRadius;
+
+            //bird akan dihancurkan segera setelah ledakan terjadi. Diberi selang waktu 0.1 detik untuk waktu efek ledakan
+            //bereaksi sebelum bird hilang. Agar bird bisa langsung hilang, maka spritenya akan langsung dinonaktifkan.
+            SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+            sprite.enabled = false;
+            _flagDestroy = true;
+            StartCoroutine(DestroyAfter(0.1f));
+        }
+
+        string tag = col.gameObject.tag;
+        
+        //jika collider yang membesar mengenai obstacle/enemy, maka obstacle/enemy tersebut akan langsung dihancurkan
+        if (tag == "Bird" || tag == "Obstacle")
+        {
+            Destroy(col.gameObject);
+        }
+
+        //hanya ditambahkan agar bird tidak sempat bergelinding lagi selama 0.1 detik setelah bird meledak
+        if (tag == "Ground")
+        {
+            Physics2D.IgnoreCollision(col.gameObject.GetComponent<Collider2D>(), Collider);
+        }
+    }
+
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public SlingShooter SlingShooter;
+    public TrailController TrailController;
+    public List<Bird> Birds;
+    public List<Enemy> Enemies;
+    private Bird _shotBird;
+    public BoxCollider2D TapCollider;
+    public int level;
+    private static int maxLevel = 2;
+
+    public Text winMessage;
+
+    private bool _isGameEnded = false;
+    private bool _isWinning = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < Birds.Count; i++)
+        {
+            Birds[i].OnBirdDestroyed += ChangeBird;
+            Birds[i].OnBirdShot += AssignTrail;
+        }
+
+        for (int i = 0; i < Enemies.Count; i++)
+        {
+            Enemies[i].OnEnemyDestroyed += CheckGameEnd;
+        }
+
+        TapCollider.enabled = false;
+        SlingShooter.InitiateBird(Birds[0]);
+        _shotBird = Birds[0];
+    }
+
+    // Update is called once per frame
+    //void Update(){}
+
+    public void ChangeBird()
+    {
+        TapCollider.enabled = false;
+
+        if (_isGameEnded)
+        {
+            return;
+        }
+
+        Birds.RemoveAt(0);
+
+        if (Birds.Count > 0)
+        {
+            SlingShooter.InitiateBird(Birds[0]);
+            _shotBird = Birds[0];
+        }
+        else
+        {
+            _isGameEnded = true;
+            EndGame();
+        }
+    }
+
+    public void CheckGameEnd(GameObject destroyedEnemy)
+    {
+        for (int i = 0; i < Enemies.Count; i++)
+        {
+            if (Enemies[i].gameObject == destroyedEnemy)
+            {
+                Enemies.RemoveAt(i);
+                break;
+            }
+        }
+
+        if (Enemies.Count == 0)
+        {
+            _isGameEnded = true;
+            _isWinning = true;
+            EndGame();
+        }
+    }
+
+    public void AssignTrail(Bird bird)
+    {
+        TrailController.SetBird(bird);
+        StartCoroutine(TrailController.SpawnTrail());
+        TapCollider.enabled = true;
+    }
+    void OnMouseUp()
+    {
+        if (_shotBird != null)
+        {
+            _shotBird.OnTap();
+        }
+    }
+
+    void EndGame()
+    {
+        if (_isGameEnded)
+        {
+            if (_isWinning)
+            {
+                Debug.Log("You Win");
+                winMessage.text = "You Win";
+                StartCoroutine(LoadScene());
+            }
+
+            else
+            {
+                Debug.Log("You Lose");
+                winMessage.text = "You Lose";
+                StartCoroutine(LoadScene());
+            }
+
+            winMessage.enabled = true;
+        }
+    }
+    
+    //Dipanggil untuk restart/ke level selanjutnya
+    private IEnumerator LoadScene()
+    {
+        yield return new WaitForSeconds(3f);
+        if (_isWinning && level < maxLevel)
+        {
+            Debug.Log("Going to next level");
+            SceneManager.LoadScene(level);
+        }
+        else if (!_isWinning)
+        {
+            Debug.Log("restarting level");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
